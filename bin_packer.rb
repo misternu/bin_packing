1d4196eeba2b22c69feec5edee4b133aa40d1d3d
Timothy Meixell <misternu@gmail.com>
2018-03-19 14:35:39 -0500

    Remove individual Item volume responsibility from BinPacker class

--- bin_packer.rb
+++ bin_packer.rb
@@ -1,11 +1,8 @@
 # Computes the ways to pack rectangular objects in rectangular bins
 class BinPacker
   def self.total_item_volume(items)
-    items.map { |item| item_volume(item) }
+    items.map { |dimensions| Item.new(dimensions: dimensions) }
+         .map(&:volume)
          .reduce(&:+)
-  end
-
-  def self.item_volume(item)
-    item.reduce(&:*) # the volume is a product of its dimensions
   end
 end
--- spec/bin_packer_spec.rb
+++ spec/bin_packer_spec.rb
@@ -1,12 +1,5 @@
 require_relative '../bin_packer'
 describe BinPacker do
-  describe '.item_volume' do
-    it 'returns the volume of the item' do
-      expect(BinPacker.item_volume([2, 2, 2])).to eq 8
-      expect(BinPacker.item_volume([1, 10, 10])).to eq 100
-      expect(BinPacker.item_volume([10, 10, 10])).to eq 1000
-    end
-  end
   describe '.total_item_volume' do
     it 'returns the total volume of the items' do
       items = [[1, 1, 1], [2, 2, 2], [10, 10, 10]]
